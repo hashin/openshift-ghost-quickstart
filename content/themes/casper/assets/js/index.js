b3bb3383a0e75dbe99ac4f94cfdd98b1ca3a10ee
Hashin Jithu <hashin.jithu@philips.com>
2015-09-29 14:13:29 +0530

    made changes

--- content/themes/casper/assets/js/index.js
+++ content/themes/casper/assets/js/index.js
@@ -2,53 +2,87 @@
  * Main JS file for Casper behaviours
  */
 
-/* globals jQuery, document */
-(function ($, undefined) {
+/*globals jQuery, document */
+(function ($) {
     "use strict";
 
-    var $document = $(document);
+    $(document).ready(function(){
 
-    $document.ready(function () {
+        $(".post-content").fitVids();
 
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
+        function casperFullImg() {
+            $("img").each( function() {
+                var contentWidth = $(".post-content").outerWidth(); // Width of the content
+                var imageWidth = $(this)[0].naturalWidth; // Original image resolution
+
+                if (imageWidth >= contentWidth) {
+                    $(this).addClass('full-img');
+                } else {
+                    $(this).removeClass('full-img');
+                }
+            });
+        };
+
+        casperFullImg();
+        $(window).smartresize(casperFullImg);
 
         $(".scroll-down").arctic_scroll();
 
-        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
-            e.preventDefault();
-            $("body").toggleClass("nav-opened nav-closed");
-        });
-
     });
 
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
+}(jQuery));
+
+(function($,sr){
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          };
+
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
+
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  }
+  // smartresize 
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
+
+// Arctic Scroll by Paul Adam Davis
+// https://github.com/PaulAdamDavis/Arctic-Scroll
+(function ($) {
     $.fn.arctic_scroll = function (options) {
 
         var defaults = {
             elem: $(this),
             speed: 500
-        },
+        };
+        var options = $.extend(defaults, options);
 
-        allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function (event) {
+        options.elem.click(function(event){     
             event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
-
+            var offset = ($(this).attr('data-offset')) ? $(this).attr('data-offset') : false,
+                position = ($(this).attr('data-position')) ? $(this).attr('data-position') : false;         
             if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+                var toMove = parseInt(offset);
+                $('html,body').stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, options.speed);
             } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+                var toMove = parseInt(position);
+                $('html,body').stop(true, false).animate({scrollTop: toMove }, options.speed);
             } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+                $('html,body').stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, options.speed);
             }
         });
 
---
+++ content/themes/oldzen/README.md
@@ -0,0 +1,44 @@
+# Uno Zen for Ghost
+
+> Minimalist and Elegant theme for Ghost.
+
+![](http://i.imgur.com/JJKXd2O.jpg)
+
+**Uno Zen** is a fork of [Uno for Ghost](https://github.com/daleanthony/Uno) that provides some features missing in the original theme:
+
+* Improve CSS layout and use a main image for each post.
+* Search support by [ghostHunter](https://github.com/i11ume/ghostHunter/).
+* Estimate the time of reading a post by [readingTime.js](https://github.com/michael-lynch/reading-time).
+* Added Progress bar with support for different themes by [pace](http://github.hubspot.com/pace/docs/welcome/).
+* Better development and production workflow with Grunt.
+
+... and more and more features in the future. If you want to help visit TODO section.
+
+## Preview
+
+![](http://i.imgur.com/YZXKWcW.png)
+![](http://i.imgur.com/6m4kKsy.png)
+![](http://i.imgur.com/BqJBN0X.jpg)
+![](http://i.imgur.com/IUtR60A.jpg)
+
+## Demo
+
+You can see a demo in my [blog](http://blog.kikobeats.com).
+
+## Installation
+
+1) Download the last release tagged version.
+
+2) Execute `npm install` and `bower install` for install the dependencies.
+
+3) For development exec `grunt`. This provide you livereload. You need to have [SASS and bourbon](https://github.com/daleanthony/uno#development) as well.
+
+4) Create your favicons with [Favicon Generator](http://realfavicongenerator.net/).
+
+4) For production exec `grunt production`. This minify and concat your code.
+
+5) Deploy!
+
+## License
+
+MIT © [Kiko Beats](kikobeats.com)
